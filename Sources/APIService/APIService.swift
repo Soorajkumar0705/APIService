995530b86f0d372ca56d2b135a434144102aaa1f
suraj kahar <surajkahar0705@gmail.com>
2025-11-14 15:06:23 +0530

    Test code added.

--- Sources/APIService/APIService.swift
+++ Sources/APIService/APIService.swift
@@ -1,2 +1,13 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+
+public class TestThisClass {
+    
+    public init() {}
+    
+    public func testThis() -> String {
+        return "Hello, world!"
+    }
+    
+}
